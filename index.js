17b9cba73bbb4da1e2239c5e01f52d63288002b9
Ben Aguirre <ben.aguirre03@gmail.com>
2023-06-29 15:43:39 -0500

    size and prinlist work

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 class LinkedList {
-    constructor(node) {
-        this.node = new Node('start')
+    constructor(head) {
+        this.head = new Node('start')
     }
 
     append(value) {
-        let node = this.node
+        let node = this.head
         while(node.next != null) {
             node = node.next
         }
         node.next = new Node(value)
+        return node
     }   
+
+    prepend(value) {
+        let holder = this.head // Holds the rest of the list so can easily change head
+        this.head = new Node(value)
+        this.head.next = holder
+        return this.head
+    }
+
+    size() {
+        let counter = 0
+        let node = this.head
+        while(node) {
+            counter++
+            if (node.next === null) {
+                console.log(counter)
+                return counter
+            }
+            node = node.next
+        }
+    }
+
+    printList() {
+        let node = this.head
+        while(node) {
+            console.log(node.value)
+            if(node.next === null) {
+                return
+            }
+            node = node.next
+        }
+        return node
+    }
 }
 
 class Node {
@@ -20,7 +53,9 @@ class Node {
 }
 
 const list = new LinkedList('list')
-list.append('two')
-list.append('third')
-list.append('four')
-console.log(list)
+list.append('Appended')
+list.prepend('Prepended')
+list.prepend('double prepended')
+list.append('at the end!')
+list.printList()
+list.size()
